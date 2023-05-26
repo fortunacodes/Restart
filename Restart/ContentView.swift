ee1dd213477a9d83fb39929cf0a38af5d282e0fc
jesusfortuna <fortuna.codes@gmail.com>
2023-05-26 23:05:50 +0200

    Initial commit: Set up project structure and basic dependencies.

---
+++ Restart/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Jesús  Fortuna on 26/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Jesús  Fortuna on 26/5/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
