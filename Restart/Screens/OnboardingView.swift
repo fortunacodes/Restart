b27ca837b666a9257b3baca40b1a56a3507eb3d1
jesusfortuna <fortuna.codes@gmail.com>
2023-05-27 22:41:12 +0200

    Drag gesture and simple animation has been added to the OnboardingView

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -14,6 +14,9 @@ struct OnboardingView: View {
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
     @State private var isAnimating: Bool = false
+    @State private var imageOffset: CGSize = CGSize(width: 0, height: 0) //.zero
+    @State private var indicatorOpacity: Double = 1.0
+    @State private var textTitle: String = "Share."
     
     //MARK: - BODY
     var body: some View {
@@ -27,10 +30,12 @@ struct OnboardingView: View {
                 Spacer()
                 
                 VStack{
-                    Text("Share.")
+                    Text(textTitle)
                         .font(.system(size: 60))
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
+                        .transition(.opacity)
+                        .id(textTitle)// This forces SwiftUI to recognize this as a new view everytime the text changes and the transition animation is triggered
                     
                     Text("""
                     It's not how much we give but
@@ -50,15 +55,50 @@ struct OnboardingView: View {
                 //MARK: - CENTER
                 ZStack {
                     CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                        .offset(x: imageOffset.width * -1)
+                        .blur(radius: abs(imageOffset.width / 5))
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                     
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
                         .opacity(isAnimating ? 1 : 0)
                         .animation(.easeOut(duration: 0.5), value: isAnimating)
-                        
-                    
+                        .offset(x: imageOffset.width * 1.2, y: 0) //1.2 creates an accelerate movement
+                        .rotationEffect(.degrees(Double(imageOffset.width / 20)))
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    if abs(imageOffset.width) <= 150 { //Abs returns a positive value making the condition works for L/R sides
+                                        imageOffset = gesture.translation
+                                        
+                                        withAnimation (.linear(duration: 0.25)) {
+                                            indicatorOpacity = 0
+                                            textTitle = "Give."
+                                        }
+                                    }
+                                })
+                                .onEnded({ _ in
+                                    imageOffset = .zero
+                                    
+                                    withAnimation (.linear(duration: 0.25)) {
+                                        indicatorOpacity = 1
+                                        textTitle = "Share."
+                                    }
+                                })
+                        )//: GESTURE
+                        .animation(.easeIn(duration: 1), value: imageOffset)
                 }//: CENTER
+                .overlay(
+                    Image(systemName: "arrow.left.and.right.circle")
+                        .font(.system(size: 44, weight: .ultraLight))
+                        .foregroundColor(.white)
+                        .offset(y: 20)
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
+                        .opacity(indicatorOpacity)
+                        ,alignment: .bottom
+                )
                 
                 Spacer()
                 
